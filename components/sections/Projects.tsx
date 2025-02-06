f18078b6aa6ed0d4d07f8d1276a1619f8786ce58
gneo0 <geo_nio@hotmail.com>
2025-02-06 23:48:13 +0200

    Add better styles on project section

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -31,13 +31,15 @@ export default async function Projects() {
             <Image
               width={150}
               height={70}
-              className="object-cover w-2/3 sm:w-96 sm:h-44"
+              className="sm:w-96 sm:h-44 object-cover w-2/3"
               src={`/${project.name}-mockup.png`}
               alt={`${project.name} photo`}
             />
             <div className="flex flex-col justify-between w-full gap-4">
               <div>
-                <h5>{project.name}</h5>
+                <h5 className="decoration-2 decoration-secondary-color mb-2 underline">
+                  {project.name}
+                </h5>
                 <p>{project.description}</p>
               </div>
               <div className="flex items-center justify-between">
@@ -54,7 +56,7 @@ export default async function Projects() {
                 <Link
                   target="_blank"
                   href={project.homepage}
-                  className="flex items-center gap-2 border border-accent-color py-1 px-2 hover:bg-accent-color hover:text-secondary-color rounded transition-colors ease-in"
+                  className="border-accent-color hover:bg-accent-color hover:text-secondary-color flex items-center gap-2 px-2 py-1 transition-colors ease-in border rounded"
                 >
                   Watch Live
                   <span>
